4100d7d0fc4240f15c34d2589c66a8088af3c8b4
AthiraKarthe <42089489+AthiraKarthe@users.noreply.github.com>
2020-08-12 17:38:45 +0530

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -1,43 +1,41 @@
-library('dplyr')
-
-features <- read.table("D:/assignment/UCI HAR Dataset/features.txt", col.names = c("n","functions"))
-activities <- read.table("D:/assignment/UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
-subject_test <- read.table("D:/assignment/UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
-x_test <- read.table("D:/assignment/UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
-y_test <- read.table("D:/assignment/UCI HAR Dataset/test/y_test.txt", col.names = "code")
-subject_train <- read.table("D:/assignment/UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
-x_train <- read.table("D:/assignment/UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
-y_train <- read.table("D:/assignment/UCI HAR Dataset/train/y_train.txt", col.names = "code")
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
 X <- rbind(x_test,x_train)
 Y <- rbind(y_test,y_train)
 Subject <- rbind(subject_test,subject_train)
-Merged_Data <- cbind(Subject,X)
-Merged_Data <- cbind(Merged_Data,Y)
-TidyData=Merged_Data %>% select(contains("subject"),contains("code"),contains("mean"), contains("std"))
+Merged <- cbind(Subject,X)
+Merged <- cbind(Merged,Y)
+Tidy=Merged %>% select(contains("subject"),contains("code"),contains("mean"), contains("std"))
 
-names(Merged_Data)
-names(TidyData)
+names(Merged)
+names(Tidy)
 
-TidyData$code<-activities[TidyData$code,2]
+Tidy$code<-activities[Tidy$code,2]
 
 
-names(TidyData)[2] = "activity"
-names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
-names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
-names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
-names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
-names(TidyData)<-gsub("^t", "Time", names(TidyData))
-names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
-names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
-names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("angle", "Angle", names(TidyData))
-names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+names(Tidy)[2] = "activity"
+names(Tidy)<-gsub("Acc", "Accelerometer", names(Tidy))
+names(Tidy)<-gsub("Gyro", "Gyroscope", names(Tidy))
+names(Tidy)<-gsub("BodyBody", "Body", names(Tidy))
+names(Tidy)<-gsub("Mag", "Magnitude", names(Tidy))
+names(Tidy)<-gsub("^t", "Time", names(Tidy))
+names(Tidy)<-gsub("^f", "Frequency", names(Tidy))
+names(Tidy)<-gsub("tBody", "TimeBody", names(Tidy))
+names(Tidy)<-gsub("-mean()", "Mean", names(Tidy), ignore.case = TRUE)
+names(Tidy)<-gsub("-std()", "STD", names(Tidy), ignore.case = TRUE)
+names(Tidy)<-gsub("-freq()", "Frequency", names(Tidy), ignore.case = TRUE)
+names(Tidy)<-gsub("angle", "Angle", names(Tidy))
+names(Tidy)<-gsub("gravity", "Gravity", names(Tidy))
 
-names(TidyData)
+names(Tidy)
 
-FinalData <- TidyData %>% group_by(subject, activity) %>% summarise_all(funs(mean))
+FinalData <- Tidy %>% group_by(subject, activity) %>% summarise_all(funs(mean))
 write.table(FinalData, "FinalData.txt", row.name=FALSE)
-FinalData
+
